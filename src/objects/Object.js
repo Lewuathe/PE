0c37e3f160dab25bb3847bc6a5bdc643d767bd05
shoe116 <shupoppo.116@gmail.com>
2012-07-30 07:26:31 +0900

    Object Basic Class

--- src/objects/Object.js
+++ src/objects/Object.js
@@ -5,7 +5,7 @@
 
 // Sphere
 var SphereIf = function(radius){
-	this.radius;
+	this.radius = radius || 1.0;
 };
 
 SphereIf.prototype = {
@@ -47,23 +47,35 @@ var Physics = function(material, shape) {
 	// public 
 	this.pos = {x:0, y:0, z:0};
 	this.pose = {px:0, py:0, pz:0};
+	this.vel = {vx:0, vy:0, vz:0};
+	this.ang_vel = {wx:0, wy:0, wz:0};
+	this.force = {fx:0, fy:0, fz:0};
+	this.torque = {tx:0, ty:0, tz:0};
+	this.dynamicsOn = true;
 	// private
 	var mass = material.density * shape.calcVolume();
-	var inertia = calcInertia(mass);
+	var inertia = shape.calcInertia(mass);
 	var reflection = material.reflection;
 };
 
-/* 
- Objects is a array of rijit bodies in a scene.
- Each rijid body has infomation of its material, shape and physics.
- Createshape functions make certain shaped rejid body and push it objects.
-*/
-var objects = [];
-
-function createSphere(_material, _sphereif){
-	var pys = new Physics(_material, _sphereif);
-	var obj = {material:_material, shape:_sphereif, pysics:pys};
-	objects.push(obs);
+Physics.prototype = {
+	// get paramaters
+	getPosition : function(){ return this.pos; };
+	    getPose : function() { return this.pose; };
+	getVelocity : function(){ return this.vel; };
+	  getAngVel : function(){ return this.ang_vel; };
+	   getForce : function() {return this.force; };
+	  getTorque : function(){ return this.torque; };
+	// set paramaters
+	setPosition : function(pos){ this.pos = pos; };
+	    setPose : function(pose){ this.pose = pose; };
+	setVelocity : function(vel){ this.vel = vel; };
+	  setAngVel : function(ang_val){ this.ang_vel = ang_val; };
+	   setForce : function(force){ this.force = force; };
+	  setTorque : function(torque){ this.torque = torque; };
+	// change dynamics
+     dynamicsEnable : function(){ dynamicsOn = true; };
+    dynamicsDisable : function(){ dynamicsOn = false; };
 };
 
 

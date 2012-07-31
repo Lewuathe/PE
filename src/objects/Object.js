6a5bfde361abab11c48d78715853603ac98a3cdd
shoe116 <shupoppo.116@gmail.com>
2012-07-31 23:52:53 +0900

    add Rijidbody Class

--- src/objects/Object.js
+++ src/objects/Object.js
@@ -33,50 +33,65 @@ var Material = function(density, reflection, color){
 	this.color = color;
 };
 
-// solid
-var Solid = function(color){
+// example
+var Stone = function(color){
 	Material.apply(this, [1.0, 1.0, color]);
 };
 
+var Sponge = function(color){
+	Material.apply(this, [0.3, 0.5, color]);
+}; 
+
 /*
- Physics for PE
- Physics has physical parameters for calculation.
+ Varialbe for Physical Engine
+ Variable has physical parameters for calculation.
 */
 
-var Physics = function(material, shape) {
+var Variable = function() { 
+	this.pos = {x:0.0, y:0.0, z:0.0};
+	this.pose = {px:0.0, py:0.0, pz:0.0};
+	this.vel = {vx:0.0, vy:0.0, vz:0.0};
+	this.ang_vel = {wx:0.0, wy:0.0, wz:0.0};
+	this.force = {fx:0.0, fy:0.0, fz:0.0};
+	this.torque = {tx:0.0, ty:0.0, tz:0.0};
+};
+
+var Rijidbody = function(_material, _shape) {
 	// public 
-	this.pos = {x:0, y:0, z:0};
-	this.pose = {px:0, py:0, pz:0};
-	this.vel = {vx:0, vy:0, vz:0};
-	this.ang_vel = {wx:0, wy:0, wz:0};
-	this.force = {fx:0, fy:0, fz:0};
-	this.torque = {tx:0, ty:0, tz:0};
+	this.variable = new Variable();
 	this.dynamicsOn = true;
 	// private
+	//
+	var material = _material;
+	var shape = _shape;
 	var mass = material.density * shape.calcVolume();
 	var inertia = shape.calcInertia(mass);
 	var reflection = material.reflection;
+	// get private paramaters
+	this.getMaterial = function(){ return material; };
+	this.getShape = function(){ return shape; };
+	this.getMass = function(){ return mass; };
+	this.getInertia = function(){ return inertia; };
+	this.getReflection = function(){ return reflection; };
 };
 
-Physics.prototype = {
-	// get paramaters
-	getPosition : function(){ return this.pos; };
-	    getPose : function() { return this.pose; };
-	getVelocity : function(){ return this.vel; };
-	  getAngVel : function(){ return this.ang_vel; };
-	   getForce : function() {return this.force; };
-	  getTorque : function(){ return this.torque; };
-	// set paramaters
-	setPosition : function(pos){ this.pos = pos; };
-	    setPose : function(pose){ this.pose = pose; };
-	setVelocity : function(vel){ this.vel = vel; };
-	  setAngVel : function(ang_val){ this.ang_vel = ang_val; };
-	   setForce : function(force){ this.force = force; };
-	  setTorque : function(torque){ this.torque = torque; };
+Rijidbody.prototype = {
+	// get variables
+	getPosition : function(){ return this.variable.pos; };
+	    getPose : function() { return this.variable.pose; };
+	getVelocity : function(){ return this.variable.vel; };
+	  getAngVel : function(){ return this.variable.ang_vel; };
+	   getForce : function() {return this.variable.force; };
+	  getTorque : function(){ return this.variable.torque; };
+	// set variables
+	setPosition : function(pos){ this.variable.pos = pos; };
+	    setPose : function(pose){ this.variable.pose = pose; };
+	setVelocity : function(vel){ this.variable.vel = vel; };
+	  setAngVel : function(ang_val){ this.variable.ang_vel = ang_val; };
+	   setForce : function(force){ this.variable.force = force; };
+	  setTorque : function(torque){ this.variable.torque = torque; };
 	// change dynamics
      dynamicsEnable : function(){ dynamicsOn = true; };
     dynamicsDisable : function(){ dynamicsOn = false; };
 };
-
-
 
--- src/objects/Scene.js
+++ src/objects/Scene.js
@@ -6,18 +6,46 @@
 
 var Scene = function(){
 	// public
-	this.objects = [];
+	this.rijidbodys = [];
+	this.time = 0.0;
 	this.gravityOn = true;
+	this.timerOn = false;
+	// private
+	var timestep = 0.1;
+	var gravity = -9.8;
+	// get private paramaters
+	this.getTimestep = function(){ return timestep; };
+	this.getGravity = function(){return gravity; };
+	this.setTimestep = function(_timestep){ timestep = _timestep; };
+	this.setGravity = function(_gravity){ gravity = _gravity; };
 };
 
 Scene.prototype = {
 	createSpere : function(_material, _sphereif){
-		var pys = new Physics(_material, _sphereif);
-		var obj = {material:_material, shape:_sphereif, pysics:pys};
-		this.objects.push(obs);
+		var rjd = new Rijidbody(_material, _sphereif);
+		this.rijidbodys.push(rjd);
 	};
 	gravityEnable : function(){ this.gravityOn = true; };
        gravityDisable : function(){ this.gravityOn = false; };
+	  timerSwitch : function(){ this.timerOn = !this.timerOn; };
+	
+	step() : function(timestep){
+		/// hogehoge ///
+	};
+	draw() : function(){
+		/// hogehoge ///
+	};
+	
+	timerFunc() : function(){ 
+		if(!this.timerOn){ return; };	
+		else{
+			var timestep = getTimestep();
+			this.step(timestep); // undifined
+			this.draw(); // undifined	
+			this.time =  + timestep;	
+		}
+	};
+	
 }
 
 
